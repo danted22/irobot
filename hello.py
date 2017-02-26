fd6876aabc2a7006267d796a75f2a2423cbdc1c9
Daniel de Sybel <daniel.de.sybel@gmail.com>
2017-02-26 18:18:26 +0000

    added buttons and formatting

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/<username>')
+@app.route('/', methods=['POST','GET'])
+@app.route('/<username>', methods=['POST','GET'])
 def hello_name(username=None):
-    return render_template("hello.html", name=username)
+    if request.method == 'POST':
+        return render_template("hello.html", name=request.form['button'])
+    else:
+        return render_template("hello.html", name=username)
