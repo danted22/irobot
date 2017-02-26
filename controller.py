e7a7e2ccb451f3b0687f666cf5dc56a411a9d323
Daniel de Sybel <daniel.de.sybel@gmail.com>
2017-02-26 22:00:34 +0000

    added Pi API

--- controller.py
+++ controller.py
@@ -1,9 +1,10 @@
+import robo_api as ra
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/', methods=['POST','GET'])
 def hello_name(username=None):
     if request.method == 'POST':
-        return render_template("controller.html", action=request.form['button'])
+        return render_template("controller.html", action=ra.Test(request.form['button']))
     else:
         return render_template("controller.html", action=None)
