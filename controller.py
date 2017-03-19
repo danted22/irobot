eacae886dee33c3d316bc3097b6696d269f14a61
Daniel de Sybel <daniel.de.sybel@gmail.com>
2017-03-19 17:09:03 +0000

    added camera support and tests

--- controller.py
+++ controller.py
@@ -1,10 +1,25 @@
 import robo_api as ra
-from flask import Flask, render_template, request
+from camera import Camera
+from flask import Flask, render_template, request, Response
 app = Flask(__name__)
 
 @app.route('/', methods=['POST','GET'])
 def hello_name(username=None):
+    """Main controlling page."""
     if request.method == 'POST':
         return render_template("controller.html", action=ra.Test(request.form['button']))
     else:
         return render_template("controller.html", action=None)
+
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
